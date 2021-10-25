7fd616c60b7d5e715cb515d86461cba3e215cf12
Arantza <arantza.beitia@gmail.com>
2021-10-25 20:16:57 +0200

    conditions

---
+++ conditional.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Conditional</title>
+</head>
+<body>
+  <?php 
+    $today = date('D');
+    print ($today == 'Mon') ? " <p>We are on monday</p>": "<p>other</p>";
+    $currentMonth = date('m');
+    $otherMonth = date('M');
+    echo ($currentMonth == '10')?"<p>we are in October</p>":"<p>($otherMonth)</p>";
+
+    $currentMinute = date("i");
+    if($currentMinute < 10) {
+      echo "<p>The current minute is less than 10</p>";
+    } elseif ($currentMinute > 15){
+      echo "<p>The current minute is grater than 15</p>";
+    }else {
+      echo "<p>Does not meet any condition</p>";
+    }
+    echo "<p>".$currentMinute."</p>";
+
+    $dayOfWeek = date('D');
+    echo $dayOfWeek;
+    switch($dayOfWeek){
+      case 'Mon':
+        print 'Monday';
+      case 'Tue':
+        print 'Tuesday';
+      case 'Wet':
+        print 'Wetnesday';
+      case 'Thu':
+        print 'Thursday';
+      case 'Fri':
+        print 'Friday';
+      case 'Sat':
+        print 'Saturday';
+      case 'Sun':
+        print 'Sunday';
+    }
+  ?>
+
+</body>
+</html>
